51a5bdc8e977e1a2e8d4425043269f6f10bc2005
Lou Lecrivain <lou.lecrivain@wdz.de>
2024-11-28 17:11:38 +0100

    dhcp: always generate dhcp-range for dnsmasq
    (configure_range is now noop)
    
    Signed-off-by: lou lecrivain <lou.lecrivain@wdz.de>

--- src/PVE/Network/SDN/Dhcp.pm
+++ src/PVE/Network/SDN/Dhcp.pm
@@ -98,11 +98,12 @@ sub regenerate_config {
 		my $subnet_config = $subnets->{$subnet_id};
 		my $dhcp_ranges = PVE::Network::SDN::Subnets::get_dhcp_ranges($subnet_config);
 
+		next if !$dhcp_ranges;
 		my ($zone, $subnet_network, $subnet_mask) = split(/-/, $subnet_id);
 		next if $zone ne $zoneid;
-		next if !$dhcp_ranges;
 
 		eval { $dhcp_plugin->configure_subnet($config, $zoneid, $vnetid, $subnet_config) };
+
 		warn "Could not configure subnet $subnet_id: $@\n" if $@;
 
 		foreach my $dhcp_range (@$dhcp_ranges) {
